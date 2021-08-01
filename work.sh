6ff3a6bb2c4df41f524c5974ab91d342c47bf149
Enzo3322 <enzo.spag14@gmail.com>
2021-08-01 20:49:09 -0300

    initial

---
+++ work.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "#################################";
+echo "       Running snap Script         ";
+echo "#################################";
+
+
+sudo snap install --classic code;
+sudo snap install spotify;
+sudo snap install discord;
+sudo snap install insomnia;
+sudo apt update && sudo apt install yarn
+
+
+
+echo "################################";
+echo "      Snap Script finished      ";
+echo "################################";
